633ceb563cffbda55aee38e613a26d03d57d610a
Wojciech Wojtak <wojciech.wojtak@maersk.com>
2021-05-24 13:40:09 +0100

    allow path after host

--- app.py
+++ app.py
@@ -11,5 +11,14 @@ def hello():
 
 @app.route("/request/<url>")
 def fetch(url):
-    response = requests.get("https://" + url, timeout=30)
-    return "{}: {}".format("https://" + url, response.elapsed.total_seconds())
+    response = requests.get("https://{}".format(url), timeout=5)
+
+    return "{}: {}".format("https://{}".format(url), response.elapsed.total_seconds())
+
+
+@app.route("/request/<url>/<path>")
+def fetch_path(url, path):
+
+    response = requests.get("https://{}/{}".format(url, path), timeout=5)
+
+    return "{}: {}".format("https://{}/{}".format(url, path), response.elapsed.total_seconds())
